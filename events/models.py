0af1fe64f4d44501ffd43d25bb03e717463e3c43
Krishna Gaur <gaurkrishna498@gmail.com>
2021-02-04 10:51:02 +0530

    Removed venue field from Event model

--- events/models.py
+++ events/models.py
@@ -8,7 +8,6 @@ class Event(models.Model):
     description = models.CharField(max_length=2000, blank=True)
     prize = models.CharField(max_length=20, blank=True)
     team_size = models.CharField(max_length=20, blank=True)
-    venue = models.CharField(max_length=50, blank=True)
     start_time = models.CharField(max_length=100, blank=False)
     end_time = models.CharField(max_length=100, blank=False)
     about = models.CharField(max_length=1500, blank=True)
