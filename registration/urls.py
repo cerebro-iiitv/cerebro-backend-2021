2f5817267b91df195c3036cbc3a68bf33836246c
Krishna Gaur <gaurkrishna498@gmail.com>
2021-02-13 12:51:41 +0530

    Created api for team registration

--- registration/urls.py
+++ registration/urls.py
@@ -1,1 +1,19 @@
+from django.db import router
 from django.urls import path
+from rest_framework import views
+from .views import index, RegisterTeamView, DashboardViewSets
+from rest_framework.routers import SimpleRouter
+from rest_framework.urlpatterns import format_suffix_patterns
+
+router = SimpleRouter()
+router.register("dashboard", DashboardViewSets,
+                basename='api-dashboard')
+
+
+urlpatterns = [
+    path('', index, name='index'),
+    path('registerteam/', RegisterTeamView.as_view(), name='registerteam')
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
+urlpatterns += router.urls
