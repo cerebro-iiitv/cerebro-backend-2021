c1403ba330f26f4d73d5f86d0f22c8c5e7bf7e98
Yash Shah <201851150@iiitvadodara.ac.in>
2021-03-06 22:51:14 +0530

    added choices in team model

--- events/models.py
+++ events/models.py
@@ -4,32 +4,29 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 class Event(models.Model):
     priority = models.IntegerField(blank=True)
-    event = models.CharField(max_length=100, blank=False)
+    title = models.CharField(max_length=100, blank=False)
     description = models.CharField(max_length=2000, blank=True)
     prize = models.CharField(max_length=20, blank=True)
     team_size = models.CharField(max_length=20, blank=True)
     start_time = models.CharField(max_length=100, blank=False)
     end_time = models.CharField(max_length=100, blank=False)
-    about = models.CharField(max_length=1500, blank=True)
     rules_doc = models.FileField(upload_to="rules", blank=True)
 
     def __str__(self):
-        return self.event
+        return self.title
 
 
 class Contact(models.Model):
 
     ROLE_CHOICES = [
-        ("Converner", "Converner"),
-        ("Co_Converner1", "Co_Converner1"),
-        ("Co_Converner2", "Co_Converner2"),
+        ("Convenor", "Convenor"),
+        ("Co_Convenor1", "Co_Convenor1"),
+        ("Co_Convenor2", "Co_Convenor2"),
         ("Member1", "Member1"),
         ("Member2", "Member2"),
     ]
 
-    event = models.ForeignKey(
-        Event, on_delete=models.CASCADE, related_name="EventContact"
-    )
+    event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name="events")
     name = models.CharField(max_length=100, blank=False)
     role = models.CharField(max_length=15, choices=ROLE_CHOICES)
     phone_number = PhoneNumberField(blank=True)
--- team/models.py
+++ team/models.py
@@ -1,17 +1,33 @@
 from django.db import models
 
 
+ROLE_CHOICES = [
+    ("Student Convener", "Student Convenor"),
+    ("Technical Lead", "Technical Lead"),
+    ("Convener", "Convener"),
+    ("Head", "Head"),
+    ("Co-Head", "Co-Head"),
+    ("Member", "Member"),
+]
+
+TEAM_CHOICES = [
+    ("Core", "Core"),
+    ("Core Support", "Core Support"),
+    ("Development", "Web Dev"),
+    ("Design", "Design"),
+    ("Video Editing", "Video Editing"),
+    ("PR", "PR"),
+    ("Gaming", "Gaming"),
+]
+
+
 class Team(models.Model):
     name = models.CharField(max_length=100, blank=False)
-    team = models.CharField(
-        max_length=100,
-    )
-    role = models.CharField(max_length=100, blank=False)
+    team = models.CharField(max_length=100, choices=TEAM_CHOICES)
+    role = models.CharField(max_length=100, blank=False, choices=ROLE_CHOICES)
     profilepic = models.ImageField(upload_to="team-profilepics", blank=True)
-    github = models.URLField(max_length=1000, blank=True)
-    linkedIn = models.URLField(max_length=1000, blank=True)
-    twitter = models.URLField(max_length=1000, blank=True)
-    dribble = models.URLField(max_length=1000, blank=True)
-
-    def __str__(self):
-        return self.team + " - " + self.role
+    portfolio = models.URLField(blank=True)
+    github = models.URLField(blank=True)
+    linked_in = models.URLField(blank=True)
+    twitter = models.URLField(blank=True)
+    dribble = models.URLField(blank=True)
