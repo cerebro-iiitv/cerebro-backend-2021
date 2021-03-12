448c716ea0a7122f44f25451b2fc415c225a3848
Krishna Gaur <gaurkrishna498@gmail.com>
2021-03-12 22:47:07 +0530

    User registration enabled on first login

--- accounts/views.py
+++ accounts/views.py
@@ -9,6 +9,7 @@ from rest_framework_simplejwt.tokens import RefreshToken
 
 from accounts.models import Account
 from accounts.serializers import AccountSerializer
+from registration.models import Dashboard
 
 def index(request):
     return render(request, "accounts/base.html")
@@ -37,10 +38,8 @@ class GoogleLogin(APIView):
         try:
             user = Account.objects.get(email=data["email"])
         except Account.DoesNotExist:
-            return Response(
-                {"Account not found": "user with given account does not exist"}
-            )
-
+            user = Account.objects.create(email=data["email"], first_name=data["given_name"], last_name=data["family_name"], 
+                                            profile_pic=data["picture"])            
         token = RefreshToken.for_user(user)
         response = {}
         response["email"] = user.email
