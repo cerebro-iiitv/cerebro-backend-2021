403236c8946b238bed5082773f92c783705a0af8
Krishna Gaur <gaurkrishna498@gmail.com>
2021-03-23 14:59:24 +0530

    Added support to show registration details through csv

--- registration/urls.py
+++ registration/urls.py
@@ -3,13 +3,16 @@ from django.urls import path
 from rest_framework.routers import SimpleRouter
 from rest_framework.urlpatterns import format_suffix_patterns
 
-from registration.views import TeamRegistrationViewSet, index
+from registration.views import TeamRegistrationViewSet, CsvGenerate, index
 
 router = SimpleRouter()
 router.register("team-register", TeamRegistrationViewSet, basename="api-team")
 
 
-urlpatterns = [path("", index, name="index")]
+urlpatterns = [
+    path("", index, name="index"),
+    path("csv-generate/<int:pk>", CsvGenerate.as_view(), name="csv-generate"),
+]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
 urlpatterns += router.urls
